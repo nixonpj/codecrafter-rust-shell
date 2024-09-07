2ce579c0917e1aa8367735c200e18022c5f74e13
Nixon Joshy <nixon@nixons-air.mynetworksettings.com>
2024-09-07 19:36:13 -0400

    Add support for command type

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,15 @@ fn main() {
             println!("{}", input[5..].trim());
             continue;
         }
+        if input.starts_with("type ") {
+            let command = input.trim_start_matches("type ");
+            if ["type", "echo", "exit"].contains(&command.trim()) {
+                println!("{} is a shell builtin", command.trim());
+            } else { 
+                println!("{} not found", command.trim());
+            }
+            continue;
+        }
         println!("{}: command not found", input.trim());
     }
 }
