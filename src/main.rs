d0fdd8a8b31171a2e9ff60de4068f7b1abc16858
Nixon Joshy <nixon@nixons-air.mynetworksettings.com>
2024-09-07 19:13:59 -0400

    Correct REPL logic

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
     // Wait for user input
     loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
